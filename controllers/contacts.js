2e2bf2a4526f65e330c70d8ebcd93d2aac2f5aed
fkordell <far18009@byui.edu>
2024-01-27 16:07:13 -0700

    w03

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -28,12 +28,52 @@ const createContact = async (req, res) => {
       birthday: req.body.birthday
     };
     const result = await mongodb.getDb().db().collection('contacts').insertOne(newContact);
-    res.setHeader('Content-Type', 'application/json');
-    res.status(201).json(result.ops[0]);
-    } catch (error) {
+    if (result.acknowledged) {
+      res.status(201).json(result);
+    } else {
+      res.status(500).send('Contact was not made');
+    }
+  } catch (error) {
     console.error('Error creating contact:', error);
     res.status(500).send('Internal Server Error');
   }
 };
 
-module.exports = { getAll, getSingle, createContact };
+const updateContact = async (req, res) => {
+  try {
+    const contactId = new ObjectId(req.params.id);
+    const updatedContact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+    const result = await mongodb.getDb().db().collection('contacts').replaceOne({ _id: contactId }, updatedContact);
+    if (result.modifiedCount === 0) {
+      res.status(500).send('Contact not found or no change made.');
+    } else {
+      res.status(204).json(result);
+    }
+  } catch (error) {
+    console.error('Error updating contact:', error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+const deleteContact = async (req, res) => {
+  try {
+    const contactId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: contactId });
+    if (result.deletedCount === 0) {
+      res.status(500).send('Contact not found or already deleted.');
+    } else {
+      res.status(200).send('Contact successfully deleted.');
+    }
+  } catch (error) {
+    console.log.error('Error deleting contact:', error);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+module.exports = { getAll, getSingle, createContact, updateContact, deleteContact };
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var app = express();
-const port = process.env.PORT || 8080;
+const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
