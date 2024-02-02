2a0ea1f6b5a5127ba8378fbd3f79497abfc2045f
fkordell <far18009@byui.edu>
2024-02-02 10:44:38 -0700

    wk04

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 const app = express();
 const port = process.env.PORT || 8080;
+const cors = require('cors');
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
